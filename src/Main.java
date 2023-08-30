3a90d94d71fb962111db5a7fe9d3f4a840fa1d0a
matrh88 <57237495+matrh88@users.noreply.github.com>
2023-08-30 09:37:13 +0200

    Added Set methods for name, age and Id

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/**
+ * Represents the application which can handle one or more students.
+ * also calculates the average age
+ *
+ * @author Matthew
+ * @version 2023-08-28
+ */
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        Student stud1 = new Student();
-        Student stud2 = new Student("Matthew", 24, "062532");
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        System.out.printf("\nStud1 Name: " + stud1.getName()
-                + " \nStud1 age: " + stud1.getAge()
-                + " \nStud1 studID: " + stud1.getStudentID() + "\n");
-        System.out.printf("\nStud2 Name: " + stud2.getName()
-                + " \nStud2 age: " + stud2.getAge()
-                + " \nStud2 studID: " + stud2.getStudentID() + "\n");
-    }
+  /**
+   * Main method.
+   *
+   * @param args for the Main method
+   */
+  public static void main(String[] args) {
+    // Press Alt+Enter with your caret at the highlighted text to see how
+    // IntelliJ IDEA suggests fixing it.
+    Student stud1 = new Student();
+    Student stud2 = new Student("Matthew", 25, "062532");
+    // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+    stud1.printInfo();
+    stud2.printInfo();
+    float avgAge = (stud1.getAge() + stud2.getAge()) / 2.0f;
+    System.out.printf("The average age of the students is: " + avgAge);
+  }
 }
--- src/Student.java
+++ src/Student.java
@@ -5,52 +5,95 @@
  * @version 2023-08-28
  */
 public class Student {
-    private String name;
-    private int age;
-    private String studentID;
+  private String name;
+  private int age;
+  private String studentId;
 
-    /**
-     * Creates an instance of Student
-     */
-    public Student(){
-        name = "Joe";
-        age = 18;
-        studentID = "ID";
-    }
+  /**
+   * Creates an instance of Student.
+   */
+  public Student() {
+    name = "Joe";
+    age = 18;
+    studentId = "000000";
+  }
 
-    /**
-     * Creates an instance of Student
-     * @param studName Students name
-     * @param studAge Students age
-     * @param studID Students ID
-     */
-    public Student(String studName, int studAge, String studID){
-        name = studName;
-        age = studAge;
-        studentID = studID;
-    }
+  /**
+   * Creates an instance of Student.
+   *
+   * @param studName Students name
+   * @param studAge  Students age
+   * @param studId   Students ID
+   */
+  public Student(String studName, int studAge, String studId) {
+    name = studName;
+    age = studAge;
+    studentId = studId;
+  }
 
-    /**
-     * gets student name
-     * @return name of the student
-     */
-    public String getName(){
-        return name;
-    }
+  /**
+   * gets student name.
+   *
+   * @return name of the student
+   */
+  public String getName() {
+    return name;
+  }
 
-    /**
-     * gets student age
-     * @return age of the student
-     */
-    public int getAge(){
-        return age;
-    }
+  /**
+   * gets student age.
+   *
+   * @return age of the student
+   */
+  public int getAge() {
+    return age;
+  }
 
-    /**
-     * gets studentID
-     * @return ID of the student
-     */
-    public String getStudentID(){
-        return studentID;
+  /**
+   * gets studentID.
+   *
+   * @return ID of the student
+   */
+  public String getStudentId() {
+    return studentId;
+  }
+
+  /**
+   * Changes the name of the student.
+   *
+   * @param newName is the new name of the student
+   */
+  public void setName(String newName) {
+    name = newName;
+  }
+
+  /**
+   * Sets the age of the student.
+   * Age must be a positive number.
+   *
+   * @param newAge age to be set. Must be a positive number
+   */
+  public void setAge(int newAge) {
+    if (newAge > 0) {
+      age = newAge;
     }
+  }
+
+  /**
+   * Sets the StudentID.
+   *
+   * @param newStudentId the ID to be set.
+   */
+  public void setId(String newStudentId) {
+    studentId = newStudentId;
+  }
+
+  /**
+   * Prints out information about the student to the console.
+   */
+  public void printInfo() {
+    System.out.printf("\nStud3 Name: " + name
+            + " \nStud3 age: " + age
+            + " \nStud3 studID: " + studentId + "\n");
+  }
 }
