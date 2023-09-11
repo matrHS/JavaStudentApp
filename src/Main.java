2ece23f7bcf6203bf539f78d00d0d638e1026825
matrh88 <57237495+matrh88@users.noreply.github.com>
2023-09-11 09:01:15 +0200

    Added StudentRegister.java

--- src/Main.java
+++ src/Main.java
@@ -25,14 +25,7 @@ public class Main {
    * @param args for the Main method
    */
   public static void main(String[] args) {
-    // Press Alt+Enter with your caret at the highlighted text to see how
-    // IntelliJ IDEA suggests fixing it.
-    Student stud1 = new Student();
-    Student stud2 = new Student("Matthew", 25, "062532");
-    // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-    printStudentInfo(stud1);
-    printStudentInfo(stud2);
-    float avgAge = (stud1.getAge() + stud2.getAge()) / 2.0f;
-    System.out.println("The average age of the students is: " + avgAge);
+
+
   }
 }
---
+++ src/StudentRegister.java
@@ -0,0 +1,20 @@
+/**
+ * Represents a register of students.
+ * <p>
+ *   The following functionality is implemented:
+ *   <ul>
+ *     <li>Search for student by name</li>
+ *     <li>Add student</li>
+ *     <li>Remove student</li>
+ *     <li>Get number of students</li>
+ *     <li>List all students</li>
+ *     <li>Get a set of students based on a criteria</li>
+ *   </ul>
+ * </p>
+ *
+ * @author Matthew
+ * @version 2023-09-11
+ */
+public class StudentRegister {
+
+}
